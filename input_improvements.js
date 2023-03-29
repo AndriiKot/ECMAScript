c2622d61fe577e5c4aab8c36cc36559740467954
AndriiKot <a_kocyuba@ukr.net>
2023-03-29 23:39:48 +0200

    Add new functional step

--- input_improvements.js
+++ input_improvements.js
@@ -4,4 +4,11 @@ inputs.forEach(item => item.addEventListener('whell',inputWheel));
 
 function inputWheel(event){
   event.preventDefault();
+   if (event.deltaY > 0) {
+    this.value = (+this.value) + 1;
+   }
+   else {
+    this.value = (+this.value) - 1;
+   }
 }
+

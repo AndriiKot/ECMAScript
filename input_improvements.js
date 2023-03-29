bd3d73095aaae3049874b02438a5ae16a0d16557
AndriiKot <a_kocyuba@ukr.net>
2023-03-29 23:28:08 +0200

    Scroll event.preventDefault();

--- input_improvements.js
+++ input_improvements.js
@@ -1,3 +1,7 @@
 ﻿const inputs = document.querySelectorAll('input[type="number"]');
 
 inputs.forEach(item => item.addEventListener('whell',inputWheel));
+
+function inputWheel(event){
+  event.preventDefault();
+}

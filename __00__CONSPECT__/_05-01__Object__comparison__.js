bdab103090aace72d907c9be054279d29b79d737
AndriiKot <a_kocyuba@ukr.net>
2024-08-01 16:43:12 +0200

    JavaScript 'JS__Conspect__' ! Object comparison  !

---
+++ __00__CONSPECT__/_05-01__Object__comparison__.js
@@ -0,0 +1,27 @@
+﻿const product1 = {
+  name: "Ball",
+  price: 20,
+};
+
+console.log(product1); // { name: 'Ball', price: 20 }
+
+const product2 = product1 
+
+// !!!!!  WARNING  !!!!!
+console.log(product1 === product2);  // true
+
+console.log(product1); // { name: 'Ball', price: 20 }
+console.log(product2); // { name: 'Ball, price: 20 }
+
+product2.name = 'Pen';
+
+console.log(product2); // { name: 'Pen', price: 20 }
+console.log(product1); // !!! { name: 'Pen', price: 20 } !!!
+
+console.log(product1 === product2);  //  !!! true !!!
+// !!!!!  WARNING END  !!!!!
+
+
+
+
+
---
+++ __00__CONSPECT__/_05-02__Object__comparison__.js
@@ -0,0 +1,22 @@
+﻿const product1 = {
+  name: "Ball",
+  price: 20,
+};
+
+const product2 = {
+  name: "Ball",
+  price: 20,
+};
+
+console.log(product1); // { name: 'Ball', price: 20 }
+console.log(product2); // { name: 'Ball', price: 20 }
+
+
+// !!!!!  WARNING  !!!!!
+console.log(product1 === product2);  // !!! false !!!
+// !!!!!  WARNING END  !!!!!
+
+
+
+
+

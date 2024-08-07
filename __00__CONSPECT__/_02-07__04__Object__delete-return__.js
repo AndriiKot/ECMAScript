0e58fda2aa1128f09a57112df09e6e0b85f17683
AndriiKot <a_kocyuba@ukr.net>
2024-08-07 23:02:20 +0200

    JavaScript 'JS__Conspect__' !  Object delete-property beginner  !

---
+++ __00__CONSPECT__/_02-07__04__Object__delete-return__.js
@@ -0,0 +1,21 @@
+﻿const product = {
+  name: "ball",
+  price: 200,
+};
+
+
+// !!! WARNING MUTATION !!!
+// !!!  return true !!!
+console.log(product);  // { name: 'ball', price: 200 }
+
+const newObject = (delete product['price']);
+
+console.log(newObject); //  !!! true !!!!
+console.log(product); // !!! { name: 'ball' }  !!!
+
+// !!! return true !!!
+// !!! WARNING MUTATION END !!!
+
+
+
+
---
+++ __00__CONSPECT__/_02-07__05__Object__delete-return__.js
@@ -0,0 +1,21 @@
+﻿const product = {
+  name: "ball",
+  price: 200,
+};
+
+
+// !!! WARNING MUTATION !!!
+// !!!  return true !!!
+console.log(product);  // { name: 'ball', price: 200 }
+
+const newObject = (delete product['width']);
+
+console.log(newObject); //  !!! true !!!!
+console.log(product); // { name: 'ball', price: 200 } 
+
+// !!! return true !!!
+// !!! WARNING MUTATION END !!!
+
+
+
+

ddd8c5b7d28d84c7056bd280f0c3c0e759a1812d
AndriiKot <a_kocyuba@ukr.net>
2024-09-05 10:02:23 +0200

    JavaScript 'JS__Conspect__' ! warning mutation object  !

---
+++ __01__WARNING__/_02__002__Object-NO-mutation__.js
@@ -0,0 +1,27 @@
+﻿const product = {
+  name: ["ball","pen"],
+  price: 200,
+};
+
+const name1 = [...product.name]; // ["ball", "pen"]
+const name2 = [...product.name]; // ["bal", "pen"]
+
+console.log(name1 === name2) // !!! false !!!
+name1.pop()
+
+console.log(name1, name2); // ["ball"] ["bal", "pen"]
+
+
+// !!! NO MUTATION !!!
+console.log(product)
+// { name: ["ball", "pen"], price: 200 } !!! NO MUTATION !!!
+// !!! NO MUTATION !!!
+
+
+
+
+
+
+
+
+

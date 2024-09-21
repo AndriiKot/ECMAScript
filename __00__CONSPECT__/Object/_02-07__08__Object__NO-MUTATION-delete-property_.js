8f3bd788fab19dc2d1635708b46ddde65ea894df
AndriiKot <a_kocyuba@ukr.net>
2024-09-21 08:45:37 +0200

    JavaScript 'JS__Conspect__' ! Objects advance !

--- __00__CONSPECT__/Object/_02-07__08__Object__NO-MUTATION-delete-property_.js
+++ __00__CONSPECT__/Object/_02-07__08__Object__NO-MUTATION-delete-property_.js
@@ -5,8 +5,15 @@
 
 
 // !!! NO MUTATION !!!
-const { price, ...newObjectProduct } = product;
+let { price, ...newObjectProduct } = product;
 
+console.log(product);          // { name: 'ball', price: 200 }
+console.log(newObjectProduct); // { name: 'ball' }
+console.log(price)             // 200
+
+price = 300;
+
+console.log('\n',price);       // 300
 console.log(product);          // { name: 'ball', price: 200 }
 console.log(newObjectProduct); // { name: 'ball' }
 
